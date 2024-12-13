a65d9fc7e779b92a1beba693a52e52bd6367fc76
aaaziim <azim210215@gmail.com>
2024-12-13 22:35:23 +0600

    Blogs Added

---
+++ src/components/Blogcard/BlogCard.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import PlaceHolderImage from "../../assets/404.jpg"
+const BlogCard = ({ blog }) => {
+    const { title, cover_image, description, published_at } = blog;
+    console.log(blog)
+    return (
+        <Link to="/" className="max-w-sm mx-auto group hover:no-underline focus:no-underline transition border-2 hover:scale-105 border-primary rounded-2xl hover:border-secondary ">
+            <img role="presentation" className="object-cover w-full  p-2 rounded-2xl h-44 
+        " src={cover_image || PlaceHolderImage} />
+            <div className="p-6 space-y-2">
+                <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">{title}</h3>
+                <span className="text-xs dark:text-gray-600">{new Date(published_at).toLocaleDateString()}</span>
+                <p>{description}</p>
+            </div>
+        </Link>
+    );
+};
+
+export default BlogCard;
